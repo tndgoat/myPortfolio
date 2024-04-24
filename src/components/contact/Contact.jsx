132058aea1a59bcda2be435c2af703d1fc1c4c1e
tndgoat <tungnd.goat@gmail.com>
2024-04-24 18:48:04 +0700

    Complete components/contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerLine } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon' />
+            <h4>Email</h4>
+            <h5>tungnd.goat@gmail.com</h5>
+            <a href="mailto:tungnd.goat@gmail.com" target='_blank'>Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon' />
+            <h4>Messenger</h4>
+            <h5>Nguyễn Duy Tùng</h5>
+            <a href="https://m.me/nguyenduytung259" target='_blank'>Send a message</a>
+          </article>
+          <article className='contact__option'>
+            <BsWhatsapp className='contact__option-icon' />
+            <h4>WhatsApp</h4>
+            <h5>+84 354304095</h5>
+            <a href="https://api.whatsapp.com/send?phone+84354304095" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
